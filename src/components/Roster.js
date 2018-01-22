08e0c71e20ae03eeae9792e81d6e7d9205432d85
shachi <bhavsarshachi@gmail.com>
2018-01-22 09:49:24 +0000

    Update Roster.js

--- src/components/Roster.js
+++ src/components/Roster.js
@@ -3,13 +3,11 @@ import { Switch, Route } from 'react-router-dom'
 import FullRoster from './FullRoster'
 import Player from './Player'
 
-// The Roster component matches one of two different routes
-// depending on the full pathname
 const Roster = () => (
-  <Switch>
+  <div className="content">
     <Route exact path='/roster' component={FullRoster}/>
     <Route path='/roster/:number' component={Player}/>
-  </Switch>
+  </div>
 )
 
 
