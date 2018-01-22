21793f58c785607ca008a4ce1d6f4f3bbc313e59
shachi <bhavsarshachi@gmail.com>
2018-01-22 10:07:04 +0000

    Update AppRoute.js

--- src/components/AppRoute.js
+++ src/components/AppRoute.js
@@ -5,22 +5,41 @@ import Home from './Home'
 import Roster from './Roster'
 
 const AppRoute = () => (
-  
-    <BrowserRouter>
-      <div>
-        <div>
-            <ul className = "header">
-              <li><NavLink to='/'>Home</NavLink></li>
-              <li><NavLink to='/roster'>Roster</NavLink></li>
-            </ul>
+    <div style = {styles.content}>
+      <BrowserRouter>
+        <div style={styles.wrapper}>
+              <ul style={styles.header}>
+                <li><NavLink to='/'>Home</NavLink></li>
+                <li><NavLink to='/roster'>Roster</NavLink></li>
+              </ul>
+          
+          <div style={styles.content}>
+              <Route exact path='/' component={Home}/>
+              <Route path='/roster' component={Roster}/>
+          </div>
         </div>
-        <div className="content">
-            <Route exact path='/' component={Home}/>
-            <Route path='/roster' component={Roster}/>
-        </div>
-      </div>
-    </BrowserRouter>
-   
+      </BrowserRouter>
+   </div>
 )
 
 export default AppRoute
+
+const styles = {
+  container: {
+      width: 400,
+      height: 400
+  },
+  wrapper: {
+      display: 'flex', 
+      justifyContent: 'center'
+  },
+  header: {
+      backgroundColor: '#23cf45',
+  },
+  content: {
+    backgroundColor: '#FFCC00',
+    width: 600,
+    display: 'flex', 
+    justifyContent: 'center'
+  }
+}
