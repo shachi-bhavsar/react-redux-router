ca30e5c31773beb402bd66c44e7037524919aa7b
shachi <bhavsarshachi@gmail.com>
2018-01-22 09:49:56 +0000

    Update Player.js

--- src/components/Player.js
+++ src/components/Player.js
@@ -2,9 +2,6 @@ import React from 'react'
 import PlayerAPI from '../api'
 import { Link } from 'react-router-dom'
 
-// The Player looks up the player using the number parsed from
-// the URL's pathname. If no player is found with the given
-// number, then a "player not found" message is displayed.
 const Player = (props) => {
   const player = PlayerAPI.get(
     parseInt(props.match.params.number, 10)
