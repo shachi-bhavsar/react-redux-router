db864780f881bee0029e9bd1d6c8c02d5a29c626
shachi <bhavsarshachi@gmail.com>
2018-01-22 09:48:35 +0000

    Update AppRoute.js

--- src/components/AppRoute.js
+++ src/components/AppRoute.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
-import AppRouter from './AppRouter'
+import { NavLink, Route } from 'react-router-dom'
+import Home from './Home'
+import Roster from './Roster'
 
 const AppRoute = () => (
   
     <BrowserRouter>
-      <AppRouter/>
+      <div>
+        <div>
+            <ul className = "header">
+              <li><NavLink to='/'>Home</NavLink></li>
+              <li><NavLink to='/roster'>Roster</NavLink></li>
+            </ul>
+        </div>
+        <div className="content">
+            <Route exact path='/' component={Home}/>
+            <Route path='/roster' component={Roster}/>
+        </div>
+      </div>
     </BrowserRouter>
    
 )
